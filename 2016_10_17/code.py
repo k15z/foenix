9f105de0c35846ae5ff0fede6167a326cad126d7
Kevin Zhang <kevz@mit.edu>
2016-10-16 19:43:07 -0400

    Added 2016_10_17

---
+++ 2016_10_17/code.py
@@ -0,0 +1,14 @@
+"""
+Author: Kevin Zhang <kevz@mit.edu>
+Date: October 14th, 2016
+"""
+
+# Write a program to read the `data.json` file and print out the name and date
+# of each news article sorted by the date it was published. The output should 
+# contain the date followed by the title; each news article should be on a new
+# line.
+# 
+# 10/16/2016 News Article Title 1
+# 10/17/2016 News Article Title 2
+# 10/17/2016 News Article Title 3
+# 10/18/2016 News Article Title 2
