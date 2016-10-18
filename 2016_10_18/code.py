a6d038814d549416b9c6fcdad70872cb9ce5afcf
Kevin Zhang <kevz@mit.edu>
2016-10-17 20:32:14 -0400

    Finalized unit tests for 2016_10_18

--- 2016_10_18/code.py
+++ 2016_10_18/code.py
@@ -16,8 +16,12 @@ class SudokuNode:
 	def __init__(self, N, dist=None):
 		"""
 		Create a new SudokuNode. If dist is None, it should be initialized with
-		a uniform probability distribution over the integers from [1,N].
+		a uniform probability distribution over the integers from [1,N]. The 
+		distribution should ALWAYS contain all N values, even if the parameter 
+		`dist` that is passed in the constructor does not contain all N values,
+		you must explicitly set them to zero.
 		"""
+		self.dist = dist # you must store the current dist in self.dist
 		raise NotImplementedError()
 
 	def add(self, node):
@@ -59,3 +63,49 @@ class SudokuNode:
 
 	def __str__(self):
 		return str(self.dist)
+
+# the below code tries to solve a 3x3 sudoku puzzle
+def solveSimpleSudoku():
+	n = [[None]*3 for _ in range(3)]
+
+	n[0][0] = SudokuNode(3, {1:0.8,2:0.1,3:0.1})
+	n[0][1] = SudokuNode(3, {1:0.6,2:0.3,3:0.1})
+	n[0][2] = SudokuNode(3)
+
+	n[1][0] = SudokuNode(3)
+	n[1][1] = SudokuNode(3)
+	n[1][2] = SudokuNode(3)
+
+	n[2][0] = SudokuNode(3)
+	n[2][1] = SudokuNode(3)
+	n[2][2] = SudokuNode(3)
+
+	n[0][0].add(n[0][1]).add(n[0][2])
+	n[0][1].add(n[0][2])
+	n[1][0].add(n[1][1]).add(n[1][2])
+	n[1][1].add(n[1][2])
+	n[2][0].add(n[2][1]).add(n[2][2])
+	n[2][1].add(n[2][2])
+
+	n[0][0].add(n[1][0]).add(n[2][0])
+	n[1][0].add(n[2][0])
+	n[0][1].add(n[1][1]).add(n[2][1])
+	n[1][1].add(n[2][1])
+	n[0][2].add(n[1][2]).add(n[2][2])
+	n[1][2].add(n[2][2])
+
+	import random
+	for _ in range(100):
+	    i = random.randint(0, 2) 
+	    j = random.randint(0, 2) 
+	    n[i][j].send()
+
+	def get_max(node):
+	    return max([(node.dist[key], key) for key in node.dist.keys()])[1]
+
+	print(get_max(n[0][0]), get_max(n[0][1]), get_max(n[0][2]))
+	print(get_max(n[1][0]), get_max(n[1][1]), get_max(n[1][2]))
+	print(get_max(n[2][0]), get_max(n[2][1]), get_max(n[2][2]))
+
+if __name__ == "__main__":
+	solveSimpleSudoku()
--- 2016_10_18/test.py
+++ 2016_10_18/test.py
@@ -3,47 +3,89 @@ from copy import deepcopy
 
 ready = True
 if ready:
-    from code import SudokuNode
+    from code import normalize, SudokuNode, solveSimpleSudoku
 else:
-    from solution import SudokuNode
+    from solution import normalize, SudokuNode, solveSimpleSudoku
 
-n = [[None]*3 for _ in range(3)]
+class TestNormalize(unittest.TestCase):
+    def test0(self):
+        dist = {"a":1,"b":2,"c":1}
+        result = normalize(dist)
 
-n[0][0] = SudokuNode(3, {1:0.8,2:0.1,3:0.1})
-n[0][1] = SudokuNode(3, {1:0.6,2:0.3,3:0.1})
-n[0][2] = SudokuNode(3)
+        # make sure dist wasn't mutated
+        self.assertEqual(dist['a'], 1)
+        self.assertEqual(dist['b'], 2)
+        self.assertEqual(dist['c'], 1)
 
-n[1][0] = SudokuNode(3)
-n[1][1] = SudokuNode(3)
-n[1][2] = SudokuNode(3)
+        # make sure dist wasn't mutated
+        self.assertAlmostEqual(result['a'], 0.25, places=3)
+        self.assertAlmostEqual(result['b'], 0.5, places=3)
+        self.assertAlmostEqual(result['c'], 0.25, places=3)
 
-n[2][0] = SudokuNode(3)
-n[2][1] = SudokuNode(3)
-n[2][2] = SudokuNode(3)
+class TestSudokuNode(unittest.TestCase):
+    def test0(self):
+        node = SudokuNode(4)
+        # make sure it uses uniform distribution
+        self.assertAlmostEqual(node.dist[1], 0.25, places=3)
+        self.assertAlmostEqual(node.dist[2], 0.25, places=3)
+        self.assertAlmostEqual(node.dist[3], 0.25, places=3)
+        self.assertAlmostEqual(node.dist[4], 0.25, places=3)
 
-n[0][0].add(n[0][1]).add(n[0][2])
-n[0][1].add(n[0][2])
-n[1][0].add(n[1][1]).add(n[1][2])
-n[1][1].add(n[1][2])
-n[2][0].add(n[2][1]).add(n[2][2])
-n[2][1].add(n[2][2])
+    def test1(self):
+        node = SudokuNode(2, {1:0.25,2:0.75})
+        # make sure it uses dist parameter
+        self.assertAlmostEqual(node.dist[1], 0.25, places=3)
+        self.assertAlmostEqual(node.dist[2], 0.75, places=3)
 
-n[0][0].add(n[1][0]).add(n[2][0])
-n[1][0].add(n[2][0])
-n[0][1].add(n[1][1]).add(n[2][1])
-n[1][1].add(n[2][1])
-n[0][2].add(n[1][2]).add(n[2][2])
-n[1][2].add(n[2][2])
+    def test2(self):
+        node = SudokuNode(2, {1:1,2:3})
+        # make sure it normalizes dist parameter
+        self.assertAlmostEqual(node.dist[1], 0.25, places=3)
+        self.assertAlmostEqual(node.dist[2], 0.75, places=3)
 
-import random
-for _ in range(100):
-    i = random.randint(0, 2) 
-    j = random.randint(0, 2) 
-    n[i][j].send()
+    def test3(self):
+        node = SudokuNode(3)
+        node.receive({1:1.0})
+        # make sure receive works properly
+        self.assertAlmostEqual(node.dist[1], 1.0, places=3)
+        self.assertAlmostEqual(node.dist[2], 0.0, places=3)
+        self.assertAlmostEqual(node.dist[3], 0.0, places=3)
 
-def get_max(node):
-    return max([(node.dist[key], key) for key in node.dist.keys()])[1]
+    def test4(self):
+        node = SudokuNode(3,{1:0.5,2:0.5,3:0.0})
+        node.receive({2:0.8,3:0.2})
+        # make sure receive works properly
+        self.assertAlmostEqual(node.dist[1], 0.0, places=3)
+        self.assertAlmostEqual(node.dist[2], 1.0, places=3)
+        self.assertAlmostEqual(node.dist[3], 0.0, places=3)
 
-print(get_max(n[0][0]), get_max(n[0][1]), get_max(n[0][2]))
-print(get_max(n[1][0]), get_max(n[1][1]), get_max(n[1][2]))
-print(get_max(n[2][0]), get_max(n[2][1]), get_max(n[2][2]))
+    def test5(self):
+        node1 = SudokuNode(3)
+        node2 = SudokuNode(3,{1:0.25,2:0.25,3:0.5})
+        node1.add(node2)
+        node1.send()
+        # make sure it sending from a node with a uniform distribution
+        # doesn't actually change anything
+        self.assertAlmostEqual(node2.dist[1], 0.25, places=3)
+        self.assertAlmostEqual(node2.dist[2], 0.25, places=3)
+        self.assertAlmostEqual(node2.dist[3], 0.5, places=3)
+
+    def test6(self):
+        node1 = SudokuNode(3,{1:1.0})
+        node2 = SudokuNode(3,{1:0.5,2:0.25,3:0.25})
+        node3 = SudokuNode(3,{1:0.25,2:0.5,3:0.25})
+        node1.add(node2).add(node3)
+        node1.send()
+
+        self.assertAlmostEqual(node2.dist[1], 0, places=3)
+        self.assertAlmostEqual(node2.dist[2], 0.5, places=3)
+        self.assertAlmostEqual(node2.dist[3], 0.5, places=3)
+
+        self.assertAlmostEqual(node3.dist[1], 0, places=3)
+        self.assertAlmostEqual(node3.dist[2], 2.0/3.0, places=3)
+        self.assertAlmostEqual(node3.dist[3], 1.0/3.0, places=3)
+
+if __name__ == '__main__':
+    unittest.main(exit=False)
+    print("Unit tests complete. Attempting to solve a 3x3 sudoku puzzle...")
+    solveSimpleSudoku()
